ba8891015c618735768ae4a8dd89ec9116a125eb
Loïc <loicguo@gmail.com>
2020-03-02 11:46:11 +0100

    connexion

--- traitement/cible_connexion.php
+++ traitement/cible_connexion.php
@@ -1,12 +1,12 @@
 <?php
 
 //Traitement des données entrées dans le form de connexion
-require '../class/user.php';
-require '../class/user_manager.php';
+require '../class/Modele/User.php';
+require '../class/Manager/Manager_User.php';
 session_start();
 
 //Vérification du compte dans la bdd
-$donnee = new User(['identifiant'=>$_POST['identifiant'],
+$donnee = new User(['email'=>$_POST['email'],
                     'mdp'=>$_POST['mdp']]);
 
 $login = new Manager;
