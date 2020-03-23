6d6a383d9413d8a74dce049e723738e7e1e961a4
Loïc <loicguo@gmail.com>
2020-03-23 10:35:40 +0100

    page de transition après inscription

--- class/Manager/Manager_User.php
+++ class/Manager/Manager_User.php
@@ -44,7 +44,7 @@ class Manager_User{
       $mail->IsHTML(true);
       $mail->Username = "quentin.lignani.schuman@gmail.com";
       $mail->Password = "Admwb2000";
-      $mail->SetFrom($inscription->getMail());
+      $mail->SetFrom($inscription->getEmail());
       $mail->Subject = "[Le Romarin] : Création de compte réussi";
       $mail->Body = "<center><b>Le Romarin</b><br><p>Bonjour ! Votre compte a été créé.</p></center></html>";
       $mail->AddAddress($inscription->getMail());
