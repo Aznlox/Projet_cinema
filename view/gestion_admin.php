cd310a70ce93774c1327f3d038ea77dcdf6a2922
Loïc <loicguo@gmail.com>
2020-05-06 17:38:11 +0200

    correctif design page

--- view/gestion_admin.php
+++ view/gestion_admin.php
@@ -12,7 +12,7 @@ else{
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-<title>Site Map - Site Map | Cinema - Free Website Template from Templates.com</title>
+<title>Gestion Admin</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="description" content="Place your description here" />
 <meta name="keywords" content="put, your, keyword, here" />
